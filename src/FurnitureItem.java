fa2282b811909de4471ae09a4fba47868f57cf49
Neeraj7979 <Imnsinha77@outlook.com>
2022-08-11 12:29:45 +0530

    Adding attributes of object furniture items

--- src/FurnitureItem.java
+++ src/FurnitureItem.java
@@ -1,5 +1,11 @@
 public class FurnitureItem {
-    
+
+    int furnitureCode;
+    String furnitureType;
+    char grade;
+    String[] color;
+    String furnitureUsage;
+    double furniturePrice;
 
 }
 
